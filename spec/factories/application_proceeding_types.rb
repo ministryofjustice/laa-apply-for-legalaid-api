1004be7e2ce1883f9013ffbdf73f9b653f452112
Abdirizak Obsiye <mrabdirizak@gmail.com>
2021-04-23 12:12:46 +0100

    Update applciation proceeding type factory
    Add a factory for proceeding types with a scope limitation to the application proceeding type factory
    
    This is because application proceeding types handles the proceeding types and not the legal aid application

--- spec/factories/application_proceeding_types.rb
+++ spec/factories/application_proceeding_types.rb
@@ -8,5 +8,16 @@ FactoryBot.define do
         create :chances_of_success, application_proceeding_type: application_proceeding_type
       end
     end
+
+    trait :with_proceeding_type_scope_limitation do
+      after(:create) do |application_proceeding_type|
+        apt = application_proceeding_type
+        pt = apt.proceeding_type
+        default_subst_sl = pt.default_substantive_scope_limitation || create(:scope_limitation, :substantive, joined_proceeding_type: pt)
+        default_df_sl = pt.default_delegated_functions_scope_limitation || create(:scope_limitation, :delegated_functions, joined_proceeding_type: pt)
+        apt.application_proceeding_types_scope_limitations << AssignedSubstantiveScopeLimitation.new(scope_limitation: default_subst_sl)
+        apt.application_proceeding_types_scope_limitations << AssignedDfScopeLimitation.new(scope_limitation: default_df_sl)
+      end
+    end
   end
 end
